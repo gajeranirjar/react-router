3b9178713bca9b2f17a5ac26e7b854d932d3e490
gajeranirjar <gnirjar1234@gmail.com>
2026-01-07 18:25:17 +0530

    fix filename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import "./app.css";
-import "./app.css";
+import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact, contactData } from "./pages/Contact";
